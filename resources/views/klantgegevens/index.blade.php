f2657623cff353b20faafbbc9e25d7e5a7aa8c4e
vanduren <dujo@summacollege.nl>
2021-07-30 23:04:30 +0200

    created layouts, sections, description:deel3.txt

---
+++ resources/views/klantgegevens/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title')
+    Lijst met klantgegevens
+@endsection
+
+@section('content')
+    <h1 class="text-gray-700">Lijst met klantgegevens</h1>
+    <p class="text-gray-500">
+        <table>
+            <thead>klantnummer</thead><thead>klantnaam</thead><thead>klantwoonplaats</thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+    </p>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" class="text-gray-900 leading-tight">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <title>Opdracht 3 - @yield('title')</title>
+</head>
+<body class="min-h-screen bg-gray-100">
+    <div class="bg-green-200 border-green-400 border-b p-4 m-4 rounded">
+        @yield('content')
+    </div>
+</body>
+</html>
