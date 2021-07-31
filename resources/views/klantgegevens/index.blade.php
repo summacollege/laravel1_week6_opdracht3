451f7dae141fb33e41bdb5e72daee4d87b1d23d3
vanduren <dujo@summacollege.nl>
2021-07-31 14:13:09 +0200

    updated index.blade van h1 naar p

--- resources/views/klantgegevens/index.blade.php
+++ resources/views/klantgegevens/index.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <h1 class="text-gray-700">Lijst met klantgegevens</h1>
+    <p class="text-gray-700">Lijst met klantgegevens</p>
     <p class="text-gray-500">
         <table>
             <thead>klantnummer</thead><thead>klantnaam</thead><thead>klantwoonplaats</thead>
